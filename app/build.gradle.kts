9650d955bd5c929c53360f592cdcfd03a63f2c9f
georg <georg@local>
2026-09-12 15:00:12 +0200

    v2.4.8: Test target for in-app update from v2.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.phoenizia.tv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 20407
-        versionName = "2.4.7"
+        versionCode = 20408
+        versionName = "2.4.8"
     }
 
     signingConfigs {
